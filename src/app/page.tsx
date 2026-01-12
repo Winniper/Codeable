6c61ab31af25c745f580ee330f68274b65eabeb9
Winniper <debadityabarman12@gmail.com>
2026-01-12 23:00:50 +0530

    added inngest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
 
 export default function Home() {
   const trpc = useTRPC()
-
-  trpc.hell.queryOptions({text: 
-    ''
-  })
+  const invoke = useMutation(trpc.invoke.mutationOptions({}))
   return (
-    <div>
+    <div className="p-4 max-w-7xl mx-auto">
+      <Button onClick={() => {invoke.mutate({text : "test@dev.com"})}}>
+        Invoke Background Jobs
+      </Button>
     </div>
   );
 }
