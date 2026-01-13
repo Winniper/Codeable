f428b612650cdee5dc7c00960caa10799cec9ef2
Winniper <debadityabarman12@gmail.com>
2026-01-13 16:52:23 +0530

    added AI background jobs

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,21 @@
+import { Agent, openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
+    const summerizer = createAgent({
+      name: "summerizer",
+      system: "You are an expert summarizer. You write readable, concise, simple content.",
+      model: openai({
+        model: "llama-3.3-70b-versatile",
+        baseUrl: "https://api.groq.com/openai/v1",
+      }),
+    });
+
+    const { output } = await summerizer.run(`Summarize the following text:${event.data.value}`)
+
+    return { output }
   },
 );
